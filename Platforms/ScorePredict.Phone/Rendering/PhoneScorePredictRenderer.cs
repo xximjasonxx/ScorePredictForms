98940eb6dc419f0c6a8f9162fcad8bdb53d24214
Jason Farrell <jfarrell@westmonroepartners.com>
2015-01-05 15:55:15 -0600

    Began adding Custom Rendered to Windows Phone

---
+++ Platforms/ScorePredict.Phone/Rendering/PhoneScorePredictRenderer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScorePredict.Phone.Rendering
+{
+    public class PhoneScorePredictRenderer : EntryRenderer
+    {
+    }
+}
