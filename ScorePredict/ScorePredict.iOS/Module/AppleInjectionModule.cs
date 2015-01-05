cf02d8f096fdce7a35e9e7cff3a4431d636355e6
Jason Farrell <imjason@gmail.com>
2015-01-04 21:16:21 -0600

    Updated using statements for Apple project

--- ScorePredict/ScorePredict.iOS/Module/AppleInjectionModule.cs
+++ ScorePredict/ScorePredict.iOS/Module/AppleInjectionModule.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using ScorePredict.Data.Services;
+using ScorePredict.iOS.Services;
 using ScorePredict.Injection;
 
 namespace ScorePredict.iOS.Module
