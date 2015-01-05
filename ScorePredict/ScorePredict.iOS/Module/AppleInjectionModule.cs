d7bd4d722ce9cbf2681fabf7736cdd9906950693
Jason Farrell <imjason@gmail.com>
2015-01-04 21:08:02 -0600

    Added Apple Version of the CreateUserService

---
+++ ScorePredict/ScorePredict.iOS/Module/AppleInjectionModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ScorePredict.Data.Services;
+using ScorePredict.Injection;
+
+namespace ScorePredict.iOS.Module
+{
+    public class AppleInjectionModule : InjectionModule
+    {
+        public AppleInjectionModule()
+        {
+            AddDependency<ICreateUserService>(typeof(AzureMobileServiceCreateUserService));
+        }
+    }
+}
