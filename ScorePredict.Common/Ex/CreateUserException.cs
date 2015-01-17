316b3943252087b234a1f5f10f4df2e1935b84b1
Jason Farrell <imjason@gmail.com>
2015-01-16 20:15:18 -0600

    Added all pages back into the application

---
+++ ScorePredict.Common/Ex/CreateUserException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScorePredict.Data.Ex
+{
+    public class CreateUserException : Exception
+    {
+        public CreateUserException(string errorMessage)
+            : base(errorMessage)
+        {
+        }
+    }
+}
---
+++ ScorePredict.Core/Pages/CreateUserPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using ScorePredict.Common.Injection;
+using ScorePredict.Data.Ex;
+using ScorePredict.Services;
+using ScorePredict.Services.Contracts;
+using Xamarin.Forms;
+
+namespace ScorePredict.Core.Pages
+{
+    public partial class CreateUserPage
+    {
+        private readonly ICreateUserService _createUserService;
+        private readonly ISaveUserSecurityService _saveUserSecurityService;
+
+        public CreateUserPage()
+        {
+            InitializeComponent();
+
+            _createUserService = Resolver.CurrentResolver.Get<ICreateUserService>();
+            _saveUserSecurityService = Resolver.CurrentResolver.Get<ISaveUserSecurityService>();
+        }
+
+        private async void CreateUser(object sender, EventArgs e)
+        {
+            var resultMessage = string.Empty;
+
+            try
+            {
+                var result = await _createUserService.CreateUserAsync(
+                    txtUsername.Text,
+                    txtPassword.Text,
+                    txtConfirm.Text);
+                if (result == null)
+                    throw new CreateUserException("An error occured creating your user. Please try again");
+
+                _saveUserSecurityService.SaveUser(result);
+                Resolver.CurrentResolver.GetInstance<IClient>().AuthenticateUser(result);
+
+                await Navigation.PopModalAsync(true);
+            }
+            catch (CreateUserException ex)
+            {
+                resultMessage = ex.Message;
+            }
+            catch
+            {
+                resultMessage = "An unknown error occurred. Please try again";
+            }
+
+            if (!string.IsNullOrEmpty(resultMessage))
+                await DisplayAlert("Error", resultMessage, "Ok");
+        }
+    }
+}
