e4d4334347b1ff6dbd184cab103539be079af9af
Jason Farrell <imjason@gmail.com>
2015-01-16 18:54:32 -0600

    Removed all files to mitigate problems with 1.3.1 upgrade

--- Platforms/ScorePredict.Touch/TouchConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace ScorePredict.Touch
-{
-    public static class TouchConstants
-    {
-        public const string UserIdKey = "UserId";
-        public const string TokenKey = "Token";
-    }
-}
--- ScorePredict.Services/IReadUserSecurityService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ScorePredict.Data;
-
-namespace ScorePredict.Services
-{
-    public interface IReadUserSecurityService
-    {
-        User ReadUser();
-    }
-}
