9471694f8493b1d97403291d042a9cb027f00c90
Jason Farrell <imjason@gmail.com>
2015-01-31 21:00:45 -0600

    UI Tweaks for Prediction view cells

---
+++ ScorePredict.Core/Converters/GameStateValueConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScorePredict.Common;
+using Xamarin.Forms;
+
+namespace ScorePredict.Core.Converters
+{
+    public class GameStateValueConverter : IValueConverter
+    {
+        private const string PregameDisplay = "Pregame";
+        private const string InProgressDisplay = "In Progress";
+        private const string FinalDisplay = "Final";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            GameState state = (GameState) value;
+            if (state == GameState.Pregame)
+                return PregameDisplay;
+
+            if (state == GameState.Final)
+                return FinalDisplay;
+
+            return InProgressDisplay;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ScorePredict.Services/Impl/Mock/MockPredictionsService.cs
+++ ScorePredict.Services/Impl/Mock/MockPredictionsService.cs
@@ -16,43 +16,43 @@ namespace ScorePredict.Services.Impl.Mock
             {
                 new Prediction()
                 {
-                    AwayPredictedScore = 12, AwayTeam = "DET", AwayTeamScore = 15, GameDay = "Mon", GameState = GameState.Final, GameTime = "8:10pm",
-                    HomePredictedScore = 10, HomeTeam = "ARI", HomeTeamScore = 7, PointsAwarded = 45
+                    AwayPredictedScore = 12, AwayTeam = "Lions", AwayTeamScore = 15, GameDay = "Mon", GameState = GameState.Final, GameTime = "8:10pm",
+                    HomePredictedScore = 10, HomeTeam = "Cardinals", HomeTeamScore = 7, PointsAwarded = 45
                 },
                 new Prediction()
                 {
-                    AwayPredictedScore = 12, AwayTeam = "NE", AwayTeamScore = 15, GameDay = "Mon", GameState = GameState.Final, GameTime = "8:10pm",
-                    HomePredictedScore = 10, HomeTeam = "SEA", HomeTeamScore = 7, PointsAwarded = 0
+                    AwayPredictedScore = 12, AwayTeam = "Patriots", AwayTeamScore = 15, GameDay = "Mon", GameState = GameState.Final, GameTime = "8:10pm",
+                    HomePredictedScore = 10, HomeTeam = "Seahawks", HomeTeamScore = 7, PointsAwarded = 0
                 },
                 new Prediction()
                 {
-                    AwayPredictedScore = 12, AwayTeam = "NYJ", AwayTeamScore = 15, GameDay = "Mon", GameState = GameState.Final, GameTime = "8:10pm",
-                    HomePredictedScore = 10, HomeTeam = "NYG", HomeTeamScore = 7, PointsAwarded = 26
+                    AwayPredictedScore = 12, AwayTeam = "Jets", AwayTeamScore = 15, GameDay = "Mon", GameState = GameState.Final, GameTime = "8:10pm",
+                    HomePredictedScore = 10, HomeTeam = "Giants", HomeTeamScore = 7, PointsAwarded = 26
                 },
                 new Prediction()
                 {
-                    AwayPredictedScore = 12, AwayTeam = "BUF", AwayTeamScore = 15, GameDay = "Mon", GameState = GameState.Final, GameTime = "8:10pm",
-                    HomePredictedScore = 10, HomeTeam = "MIA", HomeTeamScore = 7, PointsAwarded = 10
+                    AwayPredictedScore = 12, AwayTeam = "Bills", AwayTeamScore = 15, GameDay = "Mon", GameState = GameState.Final, GameTime = "8:10pm",
+                    HomePredictedScore = 10, HomeTeam = "Dolphins", HomeTeamScore = 7, PointsAwarded = 10
                 },
                 new Prediction()
                 {
-                    AwayPredictedScore = 12, AwayTeam = "DET", AwayTeamScore = 15, GameDay = "Mon", GameState = GameState.Final, GameTime = "8:10pm",
-                    HomePredictedScore = 10, HomeTeam = "ARI", HomeTeamScore = 7, PointsAwarded = 45
+                    AwayPredictedScore = 12, AwayTeam = "Lions", AwayTeamScore = 15, GameDay = "Mon", GameState = GameState.Final, GameTime = "8:10pm",
+                    HomePredictedScore = 10, HomeTeam = "Cardinals", HomeTeamScore = 7, PointsAwarded = 45
                 },
                 new Prediction()
                 {
-                    AwayPredictedScore = 12, AwayTeam = "NE", AwayTeamScore = 15, GameDay = "Mon", GameState = GameState.Final, GameTime = "8:10pm",
-                    HomePredictedScore = 10, HomeTeam = "SEA", HomeTeamScore = 7, PointsAwarded = 0
+                    AwayPredictedScore = 12, AwayTeam = "Patriots", AwayTeamScore = 15, GameDay = "Mon", GameState = GameState.Final, GameTime = "8:10pm",
+                    HomePredictedScore = 10, HomeTeam = "Seahawks", HomeTeamScore = 7, PointsAwarded = 0
                 },
                 new Prediction()
                 {
-                    AwayPredictedScore = 12, AwayTeam = "NYJ", AwayTeamScore = 15, GameDay = "Mon", GameState = GameState.Final, GameTime = "8:10pm",
-                    HomePredictedScore = 10, HomeTeam = "NYG", HomeTeamScore = 7, PointsAwarded = 26
+                    AwayPredictedScore = 12, AwayTeam = "Jets", AwayTeamScore = 15, GameDay = "Mon", GameState = GameState.Final, GameTime = "8:10pm",
+                    HomePredictedScore = 10, HomeTeam = "Giants", HomeTeamScore = 7, PointsAwarded = 26
                 },
                 new Prediction()
                 {
-                    AwayPredictedScore = 12, AwayTeam = "BUF", AwayTeamScore = 15, GameDay = "Mon", GameState = GameState.Final, GameTime = "8:10pm",
-                    HomePredictedScore = 10, HomeTeam = "MIA", HomeTeamScore = 7, PointsAwarded = 10
+                    AwayPredictedScore = 12, AwayTeam = "Bills", AwayTeamScore = 15, GameDay = "Mon", GameState = GameState.Final, GameTime = "8:10pm",
+                    HomePredictedScore = 10, HomeTeam = "Dolphins", HomeTeamScore = 7, PointsAwarded = 10
                 },
             };
         }
