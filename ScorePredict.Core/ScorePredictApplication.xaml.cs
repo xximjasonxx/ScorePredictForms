d14a8d273677f88c6cb0dcac50900e43f1d8a6c6
Jason Farrell <imjason@gmail.com>
2015-04-23 21:57:53 -0500

    Removed Login pre logic for choosing page

--- ScorePredict.Core/ScorePredictApplication.xaml.cs
+++ ScorePredict.Core/ScorePredictApplication.xaml.cs
@@ -43,39 +43,8 @@ namespace ScorePredict.Core
             builder.RegisterInstance(navigator).As<INavigator>().SingleInstance();
 
             ContainerHolder.Initialize(builder.Build());
-            var startPage = GetMainPage(ContainerHolder.Current.Resolve<IReadUserSecurityService>(),
-                ContainerHolder.Current.Resolve<IStartupService>());
-            MainPage = startPage;
-            Navigation = startPage.Navigation;
-        }
-
-        private Page GetMainPage(IReadUserSecurityService readUserSecurityService, IStartupService startupService)
-        {
-            var mainPage = new MainPage();
-            
-            User user = readUserSecurityService.ReadUser();
-            if (user == null)
-            {
-                mainPage.Navigation.PushModalAsync(new ScorePredictNavigationPage(new LoginPage())
-                {
-                    BarBackgroundColor = Color.FromHex("#3C8513"),
-                    BarTextColor = Color.FromHex("#FCD23C")
-                }, false);
-
-                return mainPage;
-            }
-
-            startupService.SetUser(user);
-            if (string.IsNullOrEmpty(user.Username))
-            {
-                mainPage.Navigation.PushModalAsync(new ScorePredictNavigationPage(new EnterUsernamePage(user))
-                {
-                    BarBackgroundColor = Color.FromHex("#3C8513"),
-                    BarTextColor = Color.FromHex("#FCD23C")
-                }, false);
-            }
-
-            return mainPage;
+            MainPage = new LoginPage();
+            Navigation = MainPage.Navigation;
         }
     }
 }
