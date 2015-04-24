d4d125e383e9d86970ae25de1670397330e8d200
Jason Farrell <imjason@gmail.com>
2015-04-23 21:48:42 -0500

    Changed the color of Progress Indicator on Windows Phone

---
+++ Platforms/ScorePredict.Phone/Rendering/PhoneActivityIndicatorRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using ScorePredict.Phone.Rendering;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WinPhone;
+using Color = Windows.UI.Color;
+
+[assembly: ExportRenderer(typeof(ActivityIndicator), typeof(PhoneActivityIndicatorRenderer))]
+namespace ScorePredict.Phone.Rendering
+{
+    public class PhoneActivityIndicatorRenderer : ActivityIndicatorRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ActivityIndicator> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                SolidColorBrush yellowColor = (SolidColorBrush) App.Current.Resources["YellowColor"];
+                this.Control.Foreground = yellowColor;
+            }
+        }
+    }
+}
