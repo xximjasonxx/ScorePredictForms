c88392445ba3ce0c00f6548f431e432a3b7f9eb7
Jason Farrell <imjason@gmail.com>
2015-05-10 21:37:48 -0500

    Fix for Display issues with Rankings page

---
+++ Platforms/ScorePredict.Touch/Rendering/TouchScorePredictListViewRenderer.cs
@@ -0,0 +1,22 @@
+using CoreGraphics;
+using ScorePredict.Core.Controls;
+using ScorePredict.Touch.Rendering;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ScorePredictListView), typeof(TouchScorePredictListViewRenderer))]
+namespace ScorePredict.Touch.Rendering
+{
+    public class TouchScorePredictListViewRenderer : ListViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                Control.TableFooterView = new UIView(CGRect.Empty);
+            }
+        }
+    }
+}
