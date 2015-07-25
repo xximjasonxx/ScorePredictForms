701e4f8fa1545c9c2aa18c3c25e090371d9c5c13
Jason Farrell <imjason@gmail.com>
2015-07-24 21:13:11 -0500

    Overrode VIewCell renderer to use a clear selected background for table cells

--- Platforms/ScorePredict.Touch/Rendering/TouchScorePredictListViewRenderer.cs
+++ Platforms/ScorePredict.Touch/Rendering/TouchScorePredictListViewRenderer.cs
@@ -7,6 +7,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
 [assembly: ExportRenderer(typeof(ScorePredictListView), typeof(TouchScorePredictListViewRenderer))]
+[assembly: ExportRenderer(typeof(ViewCell), typeof(TouchScorePredictionViewCellRenderer))]
 namespace ScorePredict.Touch.Rendering
 {
     public class TouchScorePredictListViewRenderer : ListViewRenderer
@@ -18,6 +19,20 @@ namespace ScorePredict.Touch.Rendering
             {
                 Control.TableFooterView = new UIView(CGRect.Empty);
             }
+        }
+    }
+
+    public class TouchScorePredictionViewCellRenderer : ViewCellRenderer
+    {
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            var cell = base.GetCell(item, reusableCell, tv);
+
+            UIView view = new UIView(new CGRect(0, 0, cell.Frame.Width, cell.Frame.Height));
+            view.BackgroundColor = UIColor.Clear;
+            cell.SelectedBackgroundView = view;
+
+            return cell;
         }
     }
 }
