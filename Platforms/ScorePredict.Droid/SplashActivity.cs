ea328015501c0487447b5d01f3289a35baa1e1d3
Jason Farrell <imjason@gmail.com>
2015-07-26 14:24:46 -0500

    Added splash screen to application startup (Android)

--- Platforms/ScorePredict.Droid/SplashActivity.cs
+++ Platforms/ScorePredict.Droid/SplashActivity.cs
@@ -3,7 +3,7 @@ using Android.OS;
 
 namespace ScorePredict.Droid
 {
-    [Activity(Label = "Score Predict", Icon = "@drawable/app_icon", Theme = "@style/Theme.Maintheme", MainLauncher = true)]
+    [Activity(Label = "Score Predict", Icon = "@drawable/app_icon", Theme = "@style/Theme.SplashTheme", MainLauncher = true)]
     public class SplashActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
@@ -11,8 +11,8 @@ namespace ScorePredict.Droid
             base.OnCreate(bundle);
 
             // Create your application here
-            //StartActivity(typeof(MainActivity));
-            //Finish();
+            StartActivity(typeof(MainActivity));
+            Finish();
         }
     }
 }
