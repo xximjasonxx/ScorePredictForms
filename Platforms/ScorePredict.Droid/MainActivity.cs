161a9d2731c5b24d88122b0d917d6825cfa7ee72
Jason Farrell <imjason@gmail.com>
2015-07-26 14:14:41 -0500

    Added Splash screen assets

--- Platforms/ScorePredict.Droid/MainActivity.cs
+++ Platforms/ScorePredict.Droid/MainActivity.cs
@@ -2,14 +2,13 @@
 using Android.OS;
 using ScorePredict.Core;
 using ScorePredict.Core.Contracts;
-using ScorePredict.Core.Impl;
 using ScorePredict.Services.Modules;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
 namespace ScorePredict.Droid
 {
-    [Activity(Label = "Score Predict", MainLauncher = true, Icon = "@drawable/app_icon", Theme = "@style/Theme.Maintheme")]
+    [Activity(Label = "Score Predict", Icon = "@drawable/app_icon", Theme = "@style/Theme.Maintheme")]
     public class MainActivity : FormsApplicationActivity, IKillApplication
     {
         protected override void OnCreate(Bundle bundle)
---
+++ Platforms/ScorePredict.Droid/SplashActivity.cs
@@ -0,0 +1,18 @@
+using Android.App;
+using Android.OS;
+
+namespace ScorePredict.Droid
+{
+    [Activity(Label = "Score Predict", Icon = "@drawable/app_icon", Theme = "@style/Theme.Maintheme", MainLauncher = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Create your application here
+            //StartActivity(typeof(MainActivity));
+            //Finish();
+        }
+    }
+}
