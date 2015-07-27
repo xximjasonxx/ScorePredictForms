7379bdd428871af4bfd5e312bee6558c023ded7f
Jason Farrell <imjason@gmail.com>
2015-07-26 20:40:18 -0500

    Implemented Renderer for NumberENtry

---
+++ Platforms/ScorePredict.Phone/Rendering/NumbersEntryRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Input;
+using Microsoft.Phone.Controls;
+using ScorePredict.Core.Controls;
+using ScorePredict.Phone.Rendering;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WinPhone;
+
+[assembly: ExportRenderer(typeof(NumberEntry), typeof(NumbersEntryRenderer))]
+namespace ScorePredict.Phone.Rendering
+{
+    public class NumbersEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            if (e.OldElement == null)
+            {
+                var control = (PhoneTextBox) Control.Children[0];
+                InputScope inputScope = new InputScope();
+                InputScopeName inputScopeName = new InputScopeName();
+
+                inputScopeName.NameValue = InputScopeNameValue.TelephoneNumber;
+                inputScope.Names.Add(inputScopeName);
+                control.InputScope = inputScope;
+            }
+        }
+    }
+}
---
+++ Platforms/ScorePredict.Touch/Rendering/NumberEntryRenderer.cs
@@ -0,0 +1,20 @@
+using ScorePredict.Core.Controls;
+using ScorePredict.Touch.Rendering;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(NumberEntry), typeof(NumberEntryRenderer))]
+namespace ScorePredict.Touch.Rendering
+{
+    public class NumberEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            if (e.OldElement == null)
+            {
+                Control.KeyboardType = UIKeyboardType.NumberPad;
+            }
+        }
+    }
+}
