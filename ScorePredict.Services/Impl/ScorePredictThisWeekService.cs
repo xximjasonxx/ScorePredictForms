fdfaf37a86b6f2f178de31d9a995508b9dfe9036
Jason Farrell <imjason@gmail.com>
2015-08-16 11:45:01 -0500

    Undid date hard coding for services

--- ScorePredict.Services/Impl/ScorePredictThisWeekService.cs
+++ ScorePredict.Services/Impl/ScorePredictThisWeekService.cs
@@ -22,8 +22,8 @@ namespace ScorePredict.Services.Impl
         {
             var parameters = new Dictionary<string, string>()
             {
-               //{"weekForDate", DateTime.Now.ToString("d")}
-                {"weekForDate", "9/5/2014"}
+               {"weekForDate", DateTime.Now.ToString("d")}
+               // {"weekForDate", "9/5/2014"}
             };
 
             try
